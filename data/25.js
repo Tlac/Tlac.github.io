4a88389c2117cf33bf07646b632546562d67c437
Martin Holicka <mholicka@uwaterloo.ca>
2018-04-01 21:40:12 -0400

    Removed 25 bus stop from platforms

---
+++ data/25.js
@@ -0,0 +1,27 @@
+{
+   "type":"Feature",
+   "properties":{
+      "platformNum":1,
+      "direction":"Eastbound",
+      "busName":[
+         "Waterloo/Mississauga",
+         "Waterloo/Mississauga",
+         "Waterloo/Mississauga",
+         "Waterloo/Mississauga/York University/Hwy 407/Milton"
+      ],
+      "buses":[
+         "25",
+         "25B",
+         "25C",
+         "25F"
+      ],
+      "area": "University of Waterloo"
+   },
+   "geometry":{
+      "type":"Point",
+      "coordinates":[
+         -80.54171562194823,
+         43.473091607899036
+      ]
+   }
+},
