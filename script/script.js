18d7283fddcc434a0063232db1f13ca397bc25c1
Thomas Lac <thomaslac912@gmail.com>
2022-06-19 16:11:22 -0400

    Added mobile nav bar

--- script/script.js
+++ script/script.js
@@ -1,16 +1,13 @@
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-    var x = document.getElementById("navBar");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-}
-  
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".nav-bar");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+function openNav() {
+  document.getElementById("mobileExtendedNav").style.display = "block";
+}
+function closeNav() {
+  document.getElementById("mobileExtendedNav").style.display = "none";
+}
