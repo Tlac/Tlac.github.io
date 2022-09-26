e215ee5213ccb05f23ac1af34f4808061d48e109
Thomas Lac <thomaslac912@gmail.com>
2022-09-26 16:43:43 -0400

    Updated based on feedback

--- script/script.js
+++ script/script.js
@@ -2,11 +2,14 @@ window.addEventListener("resize", resizeListener);
 function fillImage(width, height) {
     var mobile_background = document.getElementById("mobileBackground");
     var desktop_background = document.getElementById("desktopBackground");
-    var twitter_logo = document.getElementById("whiteTwitterLogo");
-    twitter_logo.style.top = `${0.87*height}px`//"550px"
-    twitter_logo.style.bottom = null
+    var twitter_logo_mobile = document.getElementById("whiteTwitterLogoMobile");
+    var twitter_logo_desktop = document.getElementById("whiteTwitterLogoDesktop");
+    twitter_logo_mobile.style.top = `${0.87*height}px`//"550px"
+    twitter_logo_mobile.style.bottom = null
+    twitter_logo_desktop.style.top = `${0.87*height}px`//"550px"
+    twitter_logo_desktop.style.bottom = null
 
-    const WIDTH_TO_HEIGHT_RATIO = 1.7712
+    const WIDTH_TO_HEIGHT_RATIO = 2.0052 // 1.7712 for original
 
     currentRatio = width/height
     if (currentRatio > WIDTH_TO_HEIGHT_RATIO) {
@@ -16,8 +19,10 @@ function fillImage(width, height) {
     } else {
         desktop_background.style.width = null
         desktop_background.style.height = "100%"
-        twitter_logo.style.top = null
-        twitter_logo.style.bottom = "6%"
+        twitter_logo_mobile.style.top = null
+        twitter_logo_mobile.style.bottom = "6%"
+        twitter_logo_desktop.style.top = null
+        twitter_logo_desktop.style.bottom = "6%"
     }
 
     if (currentRatio > WIDTH_TO_HEIGHT_RATIO) {
